961d9f18698770d1a1cd4bfdabaf9841a28f2238
Anuroop P S <anuroopps2001@gmail.com>
2026-03-29 16:35:01 +0530

    logs gathering feature added

--- 02-aks-health/cli.py
+++ 02-aks-health/cli.py
@@ -1,14 +1,32 @@
 # CLI handler
 import argparse
 from checks.pod_check import run_pod_checks  # imporing from checks folder
-
+from utils.logs import get_pod_logs
 def run():
     parser = argparse.ArgumentParser(description="AKS Health check")
-    parser.add_argument("command", choices=["scan"])
-    parser.add_argument("--namespace", default=None)
+    # 1. Create a "subparser" manager
+    subparsers = parser.add_subparsers(dest="command", required=True)
 
+    # 2. Create the 'scan' command branch
+    scan_parser = subparsers.add_parser("scan", help="Scan for issues")
+    scan_parser.add_argument("--namespace", default=None, help="Namespace to scan for issues")
+
+    # 3. Create the 'logs' command branch
+    log_parser = subparsers.add_parser("logs", help="Get pod logs")
+    log_parser.add_argument("--namespace", default="default", help="Namespace of the pod")
+
+    # Now --pod is REQUIRED, but ONLY for the 'logs' command
+    log_parser.add_argument("--pod", required=True, help="Name of the pod to inspect")
 
     args = parser.parse_args()
 
     if args.command == "scan":
+        print(f"---- Starting AKS Scan in namespace: {args.namespace or 'ALL'} ----")
         run_pod_checks(namespace=args.namespace)
+    elif args.command == "logs":
+        # The 'logs' command requires --pod, so we know it's safe to use here
+        print(f"---- Fetching logs for {args.pod} in  {args.namespace} ----")
+        if not args.pod:
+            print(f"Error: --pod is required for the logs command")
+            return
+        get_pod_logs(args.pod, args.namespace)
---
+++ 02-aks-health/utils/logs.py
@@ -0,0 +1,23 @@
+from kubernetes import client, config
+import json
+
+def get_pod_logs(pod_name, namespace):
+    config.load_config()
+
+    v1 = client.CoreV1Api()
+
+    try:
+        print(f"\n====== Logs for {namespace}/{pod_name} ======")
+        # To get the objects stored inside v1
+        print(json.dumps(v1))
+        logs = v1.read_namespaced_pod_log(
+            name=pod_name,
+            namespace=namespace,
+            tail_name=50  # Last 50 lines
+        )
+
+        print(logs)
+
+    except Exception as e:
+        print(f"Error fetching logs {e}")
+        
