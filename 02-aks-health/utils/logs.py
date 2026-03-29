22535c2ec90a12d0df5068e13ecc22eaaf827267
Anuroop P S <anuroopps2001@gmail.com>
2026-03-29 17:20:45 +0530

    added containers flag code to fetch logs from an pod

--- 02-aks-health/utils/logs.py
+++ 02-aks-health/utils/logs.py
@@ -15,31 +15,31 @@ def get_pod_logs(pod_name, namespace, target_container=None):
         for c in pod_info.spec.containers:
             containers_list.append(c.name)
 
-        # Scenario A: User didn't specify a container
-        if not target_container:
-            print(f"\n[!] Pod {pod_info.metadata.name} has {len(containers_list)} containers: {', '.join(containers_list) }")
-            print(f"Usage: python cli.py logs --pod {pod_name} --container <name>")
-            return # Exit early so we don't fetch random logs
         
         # If there's only 1 container, and if user don't provide --container, don't bother asking the user
         if not target_container and len(containers_list) == 1:
             target_container = containers_list[0]
 
-        # Scenario B: User specified a container that doesn't exist
-        elif target_container not in containers_list:
-            print(f"Error: container {target_container} not found..")
-            print(f"Available choices: {', '.join(containers_list)}")
+        if not target_container:
+            # This handles pods with 2+ containers where the user forgot --container
+            print(f"\n[!] Pod {pod_info.metadata.name} has {len(containers_list)} containers: {', '.join(containers_list)}")
+            print(f"Usage: python cli.py logs --pod {pod_name} --container <name>")
+            return 
 
-        # Scenario C: Success! Fetch the specific logs
+        if target_container not in containers_list:
+            print(f"Error: container '{target_container}' not found.")
+            print(f"Available choices: {', '.join(containers_list)}")
+            return
+
+        # 4. EXECUTION: Now that we are 100% sure target_container is valid, fetch logs
         print(f"--- Fetching logs for container: {target_container} ---")
         logs = v1.read_namespaced_pod_log(
             name=pod_name,
             namespace=namespace,
-            container = target_container,
-            tail_name=50  # Last 50 lines
+            container=target_container,
+            tail_lines=50  # Fixed from tail_name to tail_lines
         )
-
-        print(logs if logs else ["No logs available"])
+        print(logs if logs else "[No logs found]")
 
     except Exception as e:
         print(f"Error fetching logs {e}")
